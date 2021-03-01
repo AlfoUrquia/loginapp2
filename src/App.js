c1f324e573168231baee98bc7096e4267ac4b78f
AlfonsoU <alfonsou.06@gmail.com>
2021-02-28 22:07:38 -0600

    .

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+
+<div class="bg">
+    <div>
+        <div>
+        <div class="title-container">
+                <div class="flex flex-col">
+                <h1 class="title">Login Page</h1>
+                </div>
+            </div>
+            <div class="flex flex-col items-center justify-center">
+                <h2 class="">App Login</h2>
+                </div>
+            <div class="container-login">
+                    <div class="flex flex-col">
+                   <div class="my-5 flex items-center">
+                    <p class="mx-5">Usuario</p>
+                    <input type="text" placeholder="Usuario"></input>
+                </div>
+                </div>
+                <div class="flex flex-col">
+                   <div class="my-5 flex items-center">
+                    <p class="mx-5">Contraseña</p>
+                    <input type="text" placeholder="Contraseña"></input>
+                </div>
+                </div>
+                <div>
+                    <button class="logbutton">Acceder</button>
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+  );
+}
+
+export default App;
